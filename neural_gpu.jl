347e5081f64a13a80977de52d43f674df410188f
Pirah <psoomro16@ku.edu.tr>
2017-04-04 11:37:24 +0300

    init weight and CGRU revisited.this version hengerates one output state

--- neural_gpu.jl
+++ neural_gpu.jl
@@ -55,44 +55,65 @@ function init_state(E, input, vocab, w,m=24)
 	return s
 	
 end
-function initWeight(kw,kh,x,y, atype=Array{Float32}, winit=0.1, m=24, )
-	w = Array(Any,4);
-	w[1] = KnetArray{Float32}(winit*randn(kw,kh,m,m));
-	w[2] = KnetArray{Float32}(zeros(1,1,m,1));
-	X=1+floor(x-kw)
-	Y=1+floor(y-kh)
-	w[3] = KnetArray{Float32}(randn(X,Y)*winit);
-	w[4] = KnetArray{Float32}(zeros(1,1,X,1));
+function initWeight(kw,kh,x,y, vocab, atype=Array{Float32}, winit=0.1, m=24)
+	w = Array(Any,8);
+	#O=ones(m, length(vocab));
+	w[1] = KnetArray{Float32}(winit*randn(kw,kh,m,m));#U
+	w[2] = KnetArray{Float32}(zeros(1,1,m,1));#B
+	w[3] = KnetArray{Float32}(winit*randn(kw,kh,m,m));#U'
+	w[4] = KnetArray{Float32}(zeros(1,1,m,1));#B'
+	w[5] = KnetArray{Float32}(winit*randn(kw,kh,m,m));#U''
+	w[6] = KnetArray{Float32}(zeros(1,1,m,1));#B''
+	w[7] = KnetArray{Float32}(randn(m, length(vocab))*winit);#W of softmax
+	w[8] = Array{Float32}(zeros(1,length(vocab)));#b of softmax
 	return w;
 end
-function CGRU(s,w)
-	return s
+function CGRU(s,W)
+	s=reshape(s,size(s)..., 1)
+	s=convert(KnetArray{Float32}, s);
+	u=sigm(conv4(W[3], s) .+ W[4]);
+	r=sigm(conv4(W[5], s) .+ W[6]);
+	sn=u.*s+(1-u) .* tanh(conv4(W[1], (r.*s))+W[2])
+	return sn
 end
-
-function loss(sn, ygold, m=24)
-	y=zeros(length(ygold), m)
-	ypred=zeros(size(sn,2),m)# it should not be zero by the way
+function predict(s,w)
+	sn=CGRU(s,w)
+	y=Any[];
+	for k=1:size(sn,2)
+		lk=sn[1,k,:]'
+		lk=convert(KnetArray{Float32}, lk)
+		lk=lk*w[7] .+ w[8];
+		push!(y,lk);
+	end
+	return y
+end
+function loss(w, ygold, vocab, s, m=24)
+	y=Any[];#4=vocab size
+	total=0;count=0;
 	c=1;k=1
-	while c <= endof(ygold)
+	while c <= endof(ygold)#generating ygolds
+		one_hot_vector= zeros(length(vocab), 1)
 		ch=ygold[c];
-		println(ch)
+		#println(ch)
 		index= vocab[ch];
-		v=E[index]
-		y[k,:]=v;
+		one_hot_vector[index, 1]=1;
+		one_hot_vector=convert(KnetArray{Float32}, one_hot_vector)
+		push!(y,one_hot_vector);
 		c=nextind(ygold,c)
 		k=k+1
 	end
-	
-	for k=1:size(sn,2)
-	ypred(k,:)=sn[1,k,:]
+	ypred=predict(s,w)
+	for i=1: size(y,1)
+		ynorm=logp(ypred[i])
+		yg=y[i]
+		total += sum(yg .* ynorm) 
 	end
-	ynorm = logp(ypred) # ypred .- log(sum(exp(ypred),1))
-    return -sum(y .* ynorm) / size(y,2)
+	return total/size(y,1)
 	
 end
 
-function train(sn, ygold, gclip,w)
-	gloss=lossgradient(sn, ygold[1])
+function train(s, ygold, gclip, vocab, W)
+	gloss=lossgradient(s, ygold[1], vocab, W)
 	gloss2= gloss.*gloss
 	gnorm=sqrt(sum(gloss2, 1))
 	if gnorm >gclip
@@ -112,10 +133,12 @@ data=minibatch(x, ygold, 1000)
 E, vocab=embeddedMatrix();
 s0=init_state(E, x[1], vocab,3 )
 n=length(x[1]);
-w=initWeight(kh,kw, w, n)
-sn=CGRU(s0,w)#upto here it is correst
+W=initWeight(kh,kw, w, n, vocab)
+sn=CGRU(s0,W)#upto here it is correct
 lossgradient = grad(loss);
 #train(sn, ygold, gclip,w)
+l=lossgradient(W, ygold[1], vocab, s0 )
+#size(y)
 
 
 
